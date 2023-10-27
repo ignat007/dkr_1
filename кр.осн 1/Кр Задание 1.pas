861e5c3da54308fc5adc87ee94389a23e4ef0ec9
ignat007 <144335654+ignat007@users.noreply.github.com>
2023-10-27 17:52:03 +0300

    Add files via upload

---
+++ кр.осн 1/Кр Задание 1.pas
@@ -0,0 +1,15 @@
+﻿program z1;
+var x, y: real;
+begin
+   write('Введите значение x: ');
+   readln(x);
+   
+   if x < -8 then
+      y := x * x - 1 * ln(abs(x))
+   else if (x >= -8) and (x < -2) then
+      y := (x * x * x) / exp(x) - x / exp(x)
+   else
+      y := x * x * sin(x);
+      
+   writeln('Значение функции: ', y);
+end.
---
+++ кр.осн 1/Кр Задание 2.pas
@@ -0,0 +1,19 @@
+﻿program z2;
+var x, y: real;
+begin
+   x := -10;
+   
+   while x <= 0 do
+   begin
+      if x < -8 then
+         y := x * x - 1 * ln(abs(x))
+      else if (x >= -8) and (x < -2) then
+         y := (x * x * x) / exp(x) - x / exp(x)
+      else
+         y := x * x * sin(x);
+         
+      writeln('Значение функции при x = ', x, ': ', y);
+      
+      x := x + 0.3;
+   end;
+end.
